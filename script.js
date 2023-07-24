e4973dcccb4c988ef88b676810057fc54c5348cb
Davi Guerra <davialves.war@gmail.com>
2023-07-23 22:10:28 -0300

    generate fake images

---
+++ script.js
@@ -0,0 +1,7 @@
+// create a script to get images from a website
+// and save them to a folder
+
+// import the request module
+var request = require('request');
+// import the fs module
+var fs = require('fs');
